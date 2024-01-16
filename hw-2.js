4e853ffdbd3287c66a1a81a62c829d824d819b61
Kateryna Shepilova <kateryna.shepilova@md.p2h.com>
2024-01-16 21:33:59 +0200

    added hw-2

---
+++ hw-2.js
@@ -0,0 +1,7 @@
+let firstNum = +prompt('Enter number 1:');
+let secondNum = +prompt('Enter number 2:');
+let thirdNum = +prompt('Enter number 3:');
+
+let result = (firstNum + secondNum + thirdNum) / 3;
+
+alert(result);
