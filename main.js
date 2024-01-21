f3ab4b0fcabffcd344b4fc0b52dbc616bfea09e4
Kateryna Shepilova <kateryna.shepilova@md.p2h.com>
2024-01-21 16:55:13 +0200

    added hw-4

---
+++ main.js
@@ -0,0 +1,16 @@
+let firstNum = +prompt('Введіть перше число');
+let secondNum = +prompt('Введіть друге число');
+
+let resultSum = firstNum + secondNum;
+let resultDif = firstNum - secondNum;
+let resultProd = firstNum * secondNum;
+let resultQuot = firstNum / secondNum;
+
+console.log(`${firstNum} + ${secondNum} = ${resultSum}`)
+
+alert(`
+  ${firstNum} + ${secondNum} = ${resultSum} \n
+  ${firstNum} - ${secondNum} = ${resultDif} \n 
+  ${firstNum} * ${secondNum} = ${resultProd} \n 
+  ${firstNum} / ${secondNum} = ${resultQuot}`
+);
